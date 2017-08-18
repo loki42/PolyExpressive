16ed77bec72f6fb3641880420903eea975b99010
loki42 <loki.davison@gmail.com>
2017-08-18 14:54:42 +1000

    start on core loop

--- firmware/PolyExpressive.py
+++ firmware/PolyExpressive.py
@@ -1,25 +1,15 @@
 #
 # send MIDI to UART
-from machine import UART, Pin
-import machine
-from machine import Timer
+from machine import UART
+import FourWireTouch
+
 uart = UART(1, 31250)                         # init with given baudrate
 uart.init(31250, bits=8, parity=None, stop=1) # init with given parameters
+grid_x = 70
+grid_y = 99
+width_x = 420
+num_x = int(420/70)
 
-# Define which pins are connected to what
-# for resistive touch
-# wipy
-# YP = 'P20'  # must be an analog pin, ADC blue to red
-# XM = 'P19'  # must be an analog pin, ADC Green to black
-# YM = 'P22'   # can be a digital pin DAC Green to yellow
-# XP = 'P21' # can be a digital pin DAC white to orange
-ATTN_11DB = 3
-
-# wroom
-YP = 'P18'  # must be an analog pin, ADC blue to red
-XM = 'P17'  # must be an analog pin, ADC Green to black
-YM = 'P20'   # can be a digital pin DAC Green to yellow
-XP = 'P19' # can be a digital pin DAC white to orange
 
 MIDI_COMMANDS = {
         "note_on":0x80,  # Note Off
@@ -31,13 +21,13 @@ MIDI_COMMANDS = {
         "pitch_bend":0xE0   # Pich Bend
         }
 
-# on_bytes =  bytes((0x90, 0x3C, 0x7A))
-# off_bytes =  bytes((0x80, 0x3C, 0x7A))
+on_bytes =  bytes((0x90, 0x3C, 0x7A))
+off_bytes =  bytes((0x80, 0x3C, 0x7A))
 
 # for i in range(20):
-#     uart.write(on_bytes)
+#     PolyExpressive.uart.write(on_bytes)
 #     time.sleep(0.5)
-#     uart.write(off_bytes)
+#     PolyExpressive.uart.write(off_bytes)
 #     time.sleep(0.5)
 
 def send_midi_message(channel, command, data1, data2=0):
@@ -45,133 +35,69 @@ def send_midi_message(channel, command, data1, data2=0):
     uart.write(bytes((command, data1, data2)))
 
 
-# TSPoint p = ts.getPoint();
-#   // we have some minimum pressure we consider 'valid'
-#   // pressure of 0 means no pressing!
-# if (p.z > ts.pressureThreshhold) 
-
-# Resistive Touch to coordinate / pressure
-# use differentional config not single ended
-# setting time
-# 
-
-# For better pressure precision, we need to know the resistance
-# between X+ and X- Use any multimeter to read it
-# For the one we're using, its 600 ohms across the X plate
-
-# location to action
-
-# continuous 
-
-
-
-def get_point():
-    samples = [0, 0]
-    valid = True
-    NUM_SAMPLES = 2
-    _rxplate = 592
-    x = 0
-
-    y_m = Pin(YM, mode=Pin.IN)
-    y_p = Pin(YP, mode=Pin.IN)
-    adc1 = machine.ADC()
-    y_p = adc1.channel(pin=YP, attn=ATTN_11DB)
-    y_m = Pin(YM, mode=Pin.IN)
-
-    x_p = Pin(XP, mode=Pin.OUT)
-    x_m = Pin(XM, mode=Pin.OUT)
-
-    x_p.value(1)
-    x_m.value(0)
-
-    # Fast ARM chips need to allow voltages to settle
-    Timer.sleep_us(20)
-
-    for i in range(NUM_SAMPLES):
-        samples[i] = y_p()
-
-    # Allow small amount of measurement noise, because capacitive
-    # coupling to a TFT display's signals can induce some noise.
-    if ((samples[0] - samples[1] < -4) or (samples[0] - samples[1] > 4)):
-        valid = False
-    else:
-        samples[1] = (samples[0] + samples[1]) / 2 # average 2 samples
-    print("x is", samples[1])
-    x = (4095-samples[1])
-
-    # same thing but for y
-    # Timer.sleep_us(20)
-
-    x_p = Pin(XP, mode=Pin.IN)
-    x_m = Pin(XM, mode=Pin.IN)
-    adc2 = machine.ADC()
-    x_m = adc2.channel(pin=XM, attn=ATTN_11DB)
-    x_p = Pin(XP, mode=Pin.IN)
-
-
-    y_m = Pin(YM, mode=Pin.OUT)
-    y_p = Pin(YP, mode=Pin.OUT)
-
-    y_p.value(1)
-    y_m.value(0)
-
-    # Fast ARM chips need to allow voltages to settle
-    Timer.sleep_us(20)
-
-    for i in range(NUM_SAMPLES):
-        samples[i] = x_m()
-
-    # Allow small amount of measurement noise, because capacitive
-    # coupling to a TFT display's signals can induce some noise.
-    if ((samples[0] - samples[1] < -4) or (samples[0] - samples[1] > 4)):
-        valid = False
-    else:
-        samples[1] = (samples[0] + samples[1]) / 2 # average 2 samples
-    print("y is", samples[1])
-    y = (4095-samples[1])
-    # return y
-
-    # Set X+ to ground
-    # Set Y- to VCC
-    # Hi-Z X- and Y+
-    x_m = Pin(XM, mode=Pin.IN)
-    y_p = Pin(YP, mode=Pin.IN)
-
-    x_p = Pin(XP, mode=Pin.OUT)
-    y_m = Pin(YM, mode=Pin.OUT)
-    adc1 = machine.ADC()
-    y_p = adc1.channel(pin=YP, attn=ATTN_11DB)
-    adc2 = machine.ADC()
-    x_m = adc2.channel(pin=XM, attn=ATTN_11DB)
-
-    x_p.value(0)
-    y_m.value(1)
-
-    z1 = x_m()
-    z2 = y_p()
-
-    z = 0
-    if (_rxplate != 0 and z1 != 0):
-        rtouch = z2
-        rtouch /= z1
-        rtouch -= 1
-        rtouch *= x
-        rtouch *= _rxplate
-        rtouch /= 4095
-        z = rtouch
-    else:
-        z = (4095-(z2-z1))
-    if not valid:
-        print ("not valid")
-        z = 0
-    print("z is ", z)
-    return (x, y, z)
+def square_to_action(x, y, z):
+    index = int(x/grid_x) + (int(y/grid_y) * num_x)
 
 # toggle 
 #
 # update firmware
 #
-# update action list?
+# update action list / mat
+def update_mat(json_file):
+    # parse json file as the action list and mat def
+    # format is [{index:action}, {action_id:[list_of_action]}
+    # action triggers are
+    # on_start_touch shown as s
+    # on_end_touch shown as e
+    # on_change shown as c
+    # actions are
+    # send midi
+    # send midi and toggle state [on_off, on_on, initial]
+    # start sending clock
+    # stop sending clock
+    # tap temp clock
+    # start and end can use pressure (x)
+    # change can use x, y, z
+    # curve options - if no curve then takes no params
+    # cartmull rom / hermite
+    # {"x":{'curve_type':'c', v:[[in, out], ]}, "y": etc}
+    pass
+
+def execute_action(action):
+    pass
+
+
+# main loop
+def core_loop():
+    # send clock first, if we're sending clock
+    x,y,z = FourWireTouch.get_point()
+    action = square_to_action(x, y, z)
+    if z == 0: # invalid point
+        # if there is end touch queued
+        if end_action not None:
+            end_action = None
+            # run them
+            execute_action(end_action)
+    else:
+        if current_action['id'] != action['id']: # compare id's
+            # the new action isn't the same as the previous current action so execute any end_action
+            if end_action not None:
+                end_action = None
+                # run them
+                execute_action(end_action, z)
+            # got a valid point, if it's not executing at the moment then it's a start 
+            current_action = action
+            if "e" in action:
+                end_action = action["e"]
+            if "s" in action:
+                execute_action(action["s"], z)
+        else:
+            # otherwise it's an on change
+            if "c" in action:
+                execute_continous_action(action["c"], x, y, z)
+# get_point()
+# 
+
 #
 # bluetooth / wifi chaining
 #
